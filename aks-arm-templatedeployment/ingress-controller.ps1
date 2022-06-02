f8a1e41def7c4f97bd9028bc5c4b6cf941b7ac5b
Rakesh <90122976+kushagrarakesh@users.noreply.github.com>
2022-06-02 11:13:12 +0530

    Update ingress-controller.ps1

--- aks-arm-templatedeployment/ingress-controller.ps1
+++ aks-arm-templatedeployment/ingress-controller.ps1
@@ -1,6 +1,8 @@
 # Add the ingress-nginx repository
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 $RegistryName = "wpaContainerRegistry"
+$ResourceGroup = "wpa-aks-rg"
+
 
 # Set variable for ACR location to use for pulling images
 $AcrUrl = (Get-AzContainerRegistry -ResourceGroupName $ResourceGroup -Name $RegistryName).LoginServer
